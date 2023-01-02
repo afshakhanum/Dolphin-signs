3eae70840dae67aa7835ccb7885363e86edf6c72
afshan khanum <afshakhanstar@gmail.com>
2023-01-02 16:16:04 +0530

    headert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './Components/Header/Header'
 import OurWork from './Components/OurWork/OurWork'
-
 
 const App = () => {
   return (
     <div>
+      <Header />
       <OurWork />
     </div>
   )
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import './Header.css'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header-container'>
+        LOGO
+    </div>
   )
 }
 
