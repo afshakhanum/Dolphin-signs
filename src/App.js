c115b25129d9b2598acc50f7523a7f63530a3c06
afshan khanum <afshakhanstar@gmail.com>
2023-01-02 16:01:51 +0530

    installed marque

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import OurWork from './Components/OurWork/OurWork'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <OurWork />
+    </div>
   )
 }
 
--- src/Components/OurWork/OurWork.js
+++ src/Components/OurWork/OurWork.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import Marquee from "react-fast-marquee";
 const OurWork = () => {
   return (
-    <div>OurWork</div>
+    <div>
+        <Marquee
+        speed={120}
+        pauseOnHover={true}
+        >
+  I can be a React component, multiple React components, or just some text.
+  I can be a React component, multiple React components, or just some text.
+  I can be a React component, multiple React components, or just some text.
+  I can be a React component, multiple React components, or just some text.
+  I can be a React component, multiple React components, or just some text.
+  I can be a React component, multiple React components, or just some text.
+  
+</Marquee>
+    </div>
   )
 }
 
