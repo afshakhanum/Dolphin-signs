1766fccd60bae85504d8620df6fe2a2a4db88d8d
afshan khanum <afshakhanstar@gmail.com>
2023-01-03 17:33:10 +0530

    error handling

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
     return (
         <div className='card-main'>
             <div className={props.className}>
-                <img />
+                {/* <img /> */}
                 <div className='card-overlay'>
                     <p className='text'>dddd</p>
                 </div>
