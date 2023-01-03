e8c9fdb8c08159a124b35c225fd449ef1351e504
afshan khanum <afshakhanstar@gmail.com>
2023-01-03 11:41:28 +0530

    margin to body

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Card from './Components/Card/Card'
 import Header from './Components/Header/Header'
 import OurWork from './Components/OurWork/OurWork'
+import './App.css'
+import Intro from './Components/Introduction/Intro'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Intro />
       <OurWork />
       <Card />
     </div>
